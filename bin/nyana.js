15e6e71769132ce3fb0d25979792885dbd1c0c5c
Obi chinedu Frank <frankieetchty@gmail.com>
2019-09-19 00:48:42 +0100

    adding a credential manager

--- bin/nyana.js
+++ bin/nyana.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log('somethins here');
+const CredentialManager = require('../lib/credential-manager');
+
+async function main() {
+  const cred = new CredentialManager('nyana');
+  let [key, secret] = await cred.getKeyAndSecret();
+  console.log(key, secret)
+}
+
+main().catch(console.error);
